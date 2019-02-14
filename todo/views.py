5371a866534f0e25f8c975a8607a214c1f5c3154
shahanursharif <sharif@microdreamit.com>
2019-02-14 21:16:54 +0600

    rest

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework.generics import ListAPIView
-from todo.models import Todo
-from todo.serializers import TodoSerializer
-
-
-class TodoListApiView(ListAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
 
 
 def index(request):
